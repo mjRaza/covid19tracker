b6bcb30311c6463a5827874465c203cd0ee127d5
mjRaza <jaffarrazadar@gmail.com>
2020-07-08 23:11:22 +0500

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { AppBar, Typography, Button } from "@material-ui/core";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-// import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
